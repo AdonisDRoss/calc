d32ddc040701e9f6509764800d4595fca529780b
DangerFox <68658963+AdonisDRoss@users.noreply.github.com>
2020-09-02 19:02:16 -0500

    round two(added some facebook functionalities)

---
+++ back.js
@@ -0,0 +1,10 @@
+function backSpace() {
+  
+    var number = box.value;
+    
+    var len = number.length - 1;
+    
+    var newNumber = number.substring( 0, len );
+    
+    box.value = newNumber;
+}
